723d760d4112217e2095a9a34b5583068ab2d8c0
Oleg Sviridov <oleg.sviridov@approvalmax.com>
2024-11-07 15:48:34 +0100

    AP-327 client add delete form method

---
+++ Source/CurrencyCloud/Entity/Onboarding/FormDeletionResult.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace CurrencyCloud.Entity.Onboarding;
+
+public class FormDeletionResult
+{
+    /// <summary>
+    /// Message.
+    /// </summary>
+    public string Message { get; set; }
+
+    /// <summary>
+    /// Form ID.
+    /// </summary>
+    public Guid FormId { get; set; }
+}
